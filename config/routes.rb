f9aef478da9b8b711680b5ba21b0694352b2f215
RachSavoy <rachael.a.hakim@gmail.com>
2021-08-19 15:40:34 +0900

    FEAT: added partial stylesheets to index style page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'lawyer_tasks/index'
-  get 'lawyer_tasks/show'
-  get 'lawyer_tasks/edit'
-  get 'lawyer_tasks/view'
-  get 'lawyer_tasks/create'
-  get 'lawyer_tasks/destroy'
+  # get 'lawyer_tasks/index'
+  # get 'lawyer_tasks/show'
+  # get 'lawyer_tasks/edit'
+  # get 'lawyer_tasks/view'
+  # get 'lawyer_tasks/create'
+  # get 'lawyer_tasks/destroy'
   devise_for :users
-  get '/kitchensink', to: 'pages#kitchensink' if Rails.env.development?
   root to: 'pages#home'
+  get '/kitchensink', to: 'pages#kitchensink' if Rails.env.development?
   get 'info', to: 'pages#info' 
   get 'faq', to: 'pages#faq'
   get 'who', to: 'pages#who'
