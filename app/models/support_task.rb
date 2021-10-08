d7f1b6e720918d6faa4720c34a08e6f26421ef65
RachSavoy <rachael.a.hakim@gmail.com>
2021-10-08 10:40:49 +0900

    FEAT: aded link in support staff onboarding nav bar to help desk ticket.

--- app/models/support_task.rb
+++ app/models/support_task.rb
@@ -3,4 +3,4 @@ class SupportTask < ApplicationRecord
 
   def check_onboarded?
     [install_office, ring_central, adobe, vpn, dropbox, gusto, personal_info_form, tax_form, nda_form, benefits, update_social_media, email_signature, new_employee_training]
-end
+  end
